d2546c4953f30d79086733b30b54bfaceab615f5
Akshay Narayan <akshayn@mit.edu>
2022-01-08 20:59:01 +0000

    Time echoes

--- examples/client_server.rs
+++ examples/client_server.rs
@@ -4,6 +4,7 @@ use color_eyre::{eyre::WrapErr, Result};
 use dpdk_wrapper::{DpdkIoKernel, DpdkIoKernelHandle};
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::path::PathBuf;
+use std::time::{Duration, Instant};
 use structopt::StructOpt;
 use tracing::{debug, info};
 use tracing_error::ErrorLayer;
@@ -34,11 +35,11 @@ fn main() -> Result<()> {
     let (handle_s, handle_r) = flume::bounded(1);
     std::thread::spawn(move || {
         let handle = handle_r.recv().unwrap();
-
         match client {
             Some(ip) => {
-                do_client(handle, SocketAddrV4::new(ip, port)).unwrap();
-                info!("done");
+                let mut times = do_client(handle, SocketAddrV4::new(ip, port)).unwrap();
+                let (p5, p25, p50, p75, p95) = percentiles_us(&mut times);
+                info!(?p5, ?p25, ?p50, ?p75, ?p95, "done");
                 std::process::exit(0);
             }
             None => do_server(handle, port).unwrap(),
@@ -63,19 +64,23 @@ fn do_iokernel(cfg: PathBuf, handle_s: flume::Sender<DpdkIoKernelHandle>) -> Res
 }
 
 #[tracing::instrument(err, skip(handle))]
-fn do_client(handle: DpdkIoKernelHandle, remote: SocketAddrV4) -> Result<()> {
+fn do_client(handle: DpdkIoKernelHandle, remote: SocketAddrV4) -> Result<Vec<Duration>> {
     let conn = handle.socket(None)?;
     info!(?remote, "made client connection");
-    let buf = vec![97u8; 8];
-
+    let mut times = Vec::with_capacity(100);
+    let start = Instant::now();
     for i in 0..100 {
-        conn.send(remote, buf.clone()).wrap_err("send")?;
+        let msg = bincode::serialize(&TimeMsg::new(start))?;
+        conn.send(remote, msg).wrap_err("send")?;
         info!(?i, "sent");
-        let (from, _) = conn.recv().wrap_err("recv")?;
-        info!(?i, ?from, "received response");
+        let (from, buf) = conn.recv().wrap_err("recv")?;
+        let msg: TimeMsg = bincode::deserialize(&buf)?;
+        let elap = msg.elapsed(start);
+        info!(?i, ?from, ?elap, "received response");
+        times.push(elap);
     }
 
-    Ok(())
+    Ok(times)
 }
 
 #[tracing::instrument(err, skip(handle))]
@@ -87,5 +92,33 @@ fn do_server(handle: DpdkIoKernelHandle, port: u16) -> Result<()> {
         debug!(?from, "got msg");
         conn.send(from, buf).wrap_err("send echo")?;
         debug!(?from, "sent echo");
+    }
+}
+
+#[derive(Debug, Clone, Copy, serde::Serialize, serde::Deserialize)]
+struct TimeMsg(Duration, u32);
+impl TimeMsg {
+    fn new(start: Instant) -> Self {
+        Self(start.elapsed(), 0xdead)
+    }
+
+    fn elapsed(&self, start: Instant) -> Duration {
+        assert_eq!(self.1, 0xdead);
+        start.elapsed() - self.0
+    }
+}
+
+fn percentiles_us(durs: &mut [Duration]) -> (Duration, Duration, Duration, Duration, Duration) {
+    durs.sort();
+    let len = durs.len() as f64;
+    let quantile_idxs = [0.05, 0.25, 0.5, 0.75, 0.95];
+    let quantiles: Vec<_> = quantile_idxs
+        .iter()
+        .map(|q| (len * q) as usize)
+        .map(|i| durs[i])
+        .collect();
+    match quantiles[..] {
+        [p5, p25, p50, p75, p95] => (p5, p25, p50, p75, p95),
+        [..] => unreachable!(),
     }
 }
