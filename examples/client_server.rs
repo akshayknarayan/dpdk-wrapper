4a8073c5871b8d9ce236214e4a5a6676f9dbdc24
Akshay Narayan <akshayn@mit.edu>
2022-01-08 18:20:12 +0000

    client_server: dpdk ops on main thread for easier debugging

--- examples/client_server.rs
+++ examples/client_server.rs
@@ -32,22 +32,30 @@ fn main() -> Result<()> {
     let Opt { cfg, port, client } = Opt::from_args();
 
     let (handle_s, handle_r) = flume::bounded(1);
-    std::thread::spawn(move || do_iokernel(cfg, handle_s));
-    let handle = handle_r.recv().unwrap();
+    std::thread::spawn(move || {
+        let handle = handle_r.recv().unwrap();
 
-    match client {
-        Some(ip) => {
-            do_client(handle, SocketAddrV4::new(ip, port))?;
+        match client {
+            Some(ip) => {
+                do_client(handle, SocketAddrV4::new(ip, port)).unwrap();
+            }
+            None => do_server(handle, port).unwrap(),
         }
-        None => do_server(handle, port)?,
-    }
+    });
 
+    do_iokernel(cfg, handle_s)?;
     Ok(())
 }
 
 #[tracing::instrument(err, skip(cfg, handle_s))]
 fn do_iokernel(cfg: PathBuf, handle_s: flume::Sender<DpdkIoKernelHandle>) -> Result<()> {
-    let (iokernel, handle) = DpdkIoKernel::new(cfg).wrap_err("dpdk init")?;
+    let (iokernel, handle) = match DpdkIoKernel::new(cfg) {
+        Ok(x) => x,
+        Err(err) => {
+            tracing::error!(err = %format!("{:#?}", err), "Dpdk init failed");
+            return Err(err);
+        }
+    };
     handle_s.send(handle).unwrap();
     iokernel.run();
     Ok::<_, Report>(())
@@ -57,6 +65,10 @@ fn do_client(handle: DpdkIoKernelHandle, remote: SocketAddrV4) -> Result<()> {
     let conn = handle.socket(None)?;
     info!(?remote, "made client connection");
     let buf = vec![97u8; 8];
+
+    use std::io::BufRead;
+    std::io::stdin().lock().lines().next().unwrap();
+
     for i in 0..100 {
         info!(?i, "sending");
         conn.send(remote, buf.clone()).wrap_err("send")?;
